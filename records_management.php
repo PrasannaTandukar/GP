e9d6e585b64b01f7126134477d38b03cef871160
Prasanna Tandukar <prasannatandukar@pop-os.localdomain>
2022-05-08 21:05:10 +0545

    Sidebar UI

---
+++ records_management.php
@@ -0,0 +1,28 @@
+<?php include "./includes/header.php"; ?>
+
+<main class="main-record">
+    <div class="sidebar">
+        <div class="sidebar-content">
+            <div class="part-1">
+                <h3>Course Manaegement System</h3>
+                <hr>
+            </div>
+            <div class="part-2">
+                <a href="#">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
+            </div>
+            <div class="part-3">
+                <hr>
+                <button class="sidebar-btn">Sign out</button>
+            </div>
+        </div>
+    </div>
+    <div class="main-content">
+    ball
+    </div>
+</main>
+
+<?php include "./includes/footer.php" ?>
