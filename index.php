af99eb0763d34fec5a85c58dc7ed48c5c5ec2a99
Prasanna Tandukar <prasannatandukar@pop-os.localdomain>
2022-05-08 15:14:53 +0545

    Folder restructured

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php 
-echo"Hello world"
-?>
+<?php include "./includes/header.php" ?>
+
+<div class="home">
+    <div class="home-container">
+        <form action="" method="post" class="home-login">
+            <img class="uni-logo" src="./images/uni-logo.jpg" alt="university logo">
+            <h1 class="college-name" >WCU</h1>
+            <p class="college-name">Sign in with your <strong>username</strong> and <strong>password</strong></p>
+            <input type="text" name="username" placeholder="Your username">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" name="submit" value="Sign in" class="home-btn">
+        </form>
+    </div>
+</div>
+
+<?php include "./includes/footer.php" ?>
