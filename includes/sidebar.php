e40a5904d9a333a933638c709ebdc736313b4b8a
Prasanna Tandukar <prasannatandukar@pop-os.localdomain>
2022-05-09 08:53:31 +0545

    Refactored sidebar code

---
+++ includes/sidebar.php
@@ -0,0 +1,19 @@
+<div class="sidebar">
+    <div class="sidebar-content">
+        <div class="part-1">
+            <h3>Course Manaegement System</h3>
+            <hr>
+        </div>
+        <div class="part-2">
+            <a href="#">1</a>
+            <a href="#">2</a>
+            <a href="#">3</a>
+            <a href="#">4</a>
+            <a href="#">5</a>
+        </div>
+        <div class="part-3">
+            <hr>
+            <button class="sidebar-btn">Sign out</button>
+        </div>
+    </div>
+</div>
