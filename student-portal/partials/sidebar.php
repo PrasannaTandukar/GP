3c427f7a678e9a8601337ce1139d7a3d7d7df942
Prasanna Tandukar <pratandukar@gmail.com>
2022-06-02 15:16:44 +0545

    homepage

---
+++ student-portal/partials/sidebar.php
@@ -0,0 +1,15 @@
+<div class="sidebar">
+    <div class="sidebar-content">
+        <div class="part-1">
+            <h3>Student Portal</h3>
+            <hr>
+        </div>
+        <div class="part-2">
+            <a href="./student_portal.php">Home</a>
+        </div>
+        <div class="part-3">
+            <hr>
+            <button class="sidebar-btn"><a href="../includes/logout.php">Sign out</a></button>
+        </div>
+    </div>
+</div>
--- student-portal/student_portal.php
+++ student-portal/student_portal.php
@@ -3,11 +3,54 @@
 <?php
     include "../includes/check_session_student.php";
     include "../includes/db.php";
+    include "../includes/Student.php";
 ?>
 
-<?php include "../includes/header.php"; ?>
+<?php
+    $result = Student::getSingleStudent($_SESSION['username']);
+?>
 
-<h1>Hi</h1>
+<?php include "./partials/header.php"; ?>
+
+<main class="main-record">
+    <?php include "./partials/sidebar.php" ?>
+    <div class="main-content">
+        <div class="table-container">
+            <div class="top-table">
+                <h1>Student</h1>
+            </div>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Gender</th>
+                    <th>Contact</th>
+                    <th>Email</th>
+                    <th>Country</th>
+                    <th>city</th>
+                    <th>DOB</th>
+                    <th>Date Registered</th>
+                    <th>Course_id</th>
+                </tr>
+                <!-- Code to insert each row fetched from students table -->
+                <?php
+                    while($row = mysqli_fetch_assoc($result)) {
+                        ?>
+                        <tr>
+                        <?php
+                        foreach ($row as $value) {
+                            echo "<th style='font-weight: normal;'>{$value}</th>";
+                        }
+                        ?>
+                        </tr>
+                        <?php
+                    }
+                ?>
+            </table>
+        </div>
+    </div>
+</main>
 
 
 <?php include "../includes/footer.php" ?>
