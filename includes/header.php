2a74c0d6cca7c0815eb91186144bc5bbd2d1c51f
Prasanna Tandukar <pratandukar@gmail.com>
2022-06-17 10:17:45 +0545

    icon bug

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Line CSS file -->
     <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="../css/style.css">
     <title>Course Management System</title>
 </head>
